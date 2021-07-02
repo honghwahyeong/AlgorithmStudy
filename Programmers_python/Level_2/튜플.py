34aebd72e6e9720a284ddec2d011b7ecf263f704
honghwahyeong <ghdghkgud@dgu.ac.kr>
2021-07-02 19:23:14 +0900

    sql 공부

---
+++ Programmers_python/Level_2/튜플.py
@@ -0,0 +1,18 @@
+def solution(s):
+    save = {}
+    answer = []
+    s = s[1:-2]
+    s = s.split('},')
+    length = len(s)
+    for i in s:
+        i = i.split('{')
+        j = list(map(int, i[1].split(',')))
+        for k in j:
+            if k not in save:
+                save[k] = 1
+            else:
+                save[k] += 1
+    new_dict = sorted(save.items(), key=lambda x: x[1], reverse=True)
+    for k, v in new_dict:
+        answer.append(k)
+    return answer
