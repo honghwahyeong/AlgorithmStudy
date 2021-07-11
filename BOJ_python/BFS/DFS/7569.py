2cf6be07379f21080e4c3d8b9d2e1836a1ea84a9
honghwahyeong <ghdghkgud@dgu.ac.kr>
2021-07-11 23:31:10 +0900

    BOJ BFS/DFS

---
+++ BOJ_python/BFS/DFS/7569.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+dx = [-1, 1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
+
+M, N, H = map(int, input().split())
+board = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
+max_day = 0
+
+
+def bfs(x, y, z):
+    queue = deque()
+    queue.append((x, y, z))
+
+    while queue:
+        a, b, c = queue.popleft()
+        for i in range(6):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            nz = c + dz[i]
+            if 0 <= nx < M and 0 <= ny < N and 0 <= nz < H:
+                if board[nz][ny][nx] == 0:
+                    queue.append((nx, ny, nz))
+                    board[nz][ny][nx] = board[c][b][a] + 1
+
+
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if board[i][j][k] == 1:
+                bfs(k, j, i)
+flag = 0
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if board[i][j][k] == 0:
+                flag = 1
+            max_day = max(max_day, board[i][j][k])
+if flag == 0:
+    print(max_day-1)
+else:
+    print(-1)
