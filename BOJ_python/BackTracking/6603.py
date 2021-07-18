76cf5a1e0aef233a193b6fc3edfd22b979cb581f
honghwahyeong <ghdghkgud@dgu.ac.kr>
2021-07-18 15:43:10 +0900

    BOJ

---
+++ BOJ_python/BackTracking/6603.py
@@ -0,0 +1,20 @@
+
+def dfs(cnt, depth):
+    if depth == 6:
+        for i in range(6):
+            print(lucky[i], end=' ')
+        print()
+        return
+    for i in range(cnt, len(board)):
+        lucky[depth] = board[i]
+        dfs(i+1, depth+1)
+
+
+lucky = [0 for _ in range(13)]
+while 1:
+    board = list(map(int, input().split()))
+    if board[0] == 0:
+        break
+    board = board[1:]
+
+    dfs(0, 0)
