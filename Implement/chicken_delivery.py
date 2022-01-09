b21a2f50985b740c9780c657ab233d9b24a4b8d1
홍화형 <honghwahyeong@honghwahyeong-ui-MacBookPro.local>
2022-01-09 18:55:05 +0900

    chicken delivery

--- Implement/chicken_delivery.py
+++ Implement/chicken_delivery.py
@@ -1,16 +1,29 @@
 from itertools import combinations
 
+
 n, m = map(int, input().split())
 city = []
 chicken_store = []
 
-for _ in range(n):
+for i in range(n):
     data = list(map(int, input().split()))
-    city.append(data)
-
-for i in range(len(city)):
-    for j in range(len(city)):
-        if city[i][j] == 2:
+    for j in range(n):
+        if data[j] == 1:
+            city.append((i, j))
+        elif data[j] == 2:
             chicken_store.append((i, j))
 
 chicken_store_combi = list(combinations(chicken_store, m))
+
+shortest = int(1e9)
+
+for store_set in chicken_store_combi:
+    ssum = 0
+    for house_x, house_y in city:
+        temp = int(1e9)
+        for chicken_x, chicken_y in store_set:
+            temp = min(temp, abs(house_x - chicken_x)+abs(house_y - chicken_y))
+        ssum += temp
+    shortest = min(ssum, shortest)
+
+print(shortest)
