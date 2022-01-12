9ac98ea25c83f77c9fdc7124d239e38e1aed1554
honghwahyeong <ghdghkgud@dgu.ac.kr>
2022-01-12 15:40:50 +0900

    18352

---
+++ BOJ_python/BFS/DFS/18352.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+distance = [-1] * (n+1)
+distance[x] = 0
+
+queue = deque()
+queue.append(x)
+
+while queue:
+    now = queue.popleft()
+    for next in graph[now]:
+        if distance[next] == -1:
+            distance[next] = distance[now] + 1
+            queue.append(next)
+
+check = False
+for i in range(1, n+1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if not check:
+    print(-1)
