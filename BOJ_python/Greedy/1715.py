b653b9399ddef83d9e26eae8f253a41ba17d71cc
honghwahyeong <ghdghkgud@dgu.ac.kr>
2022-01-16 17:39:34 +0900

    1715

---
+++ BOJ_python/Greedy/1715.py
@@ -0,0 +1,16 @@
+from queue import PriorityQueue
+
+n = int(input())
+cards = PriorityQueue(maxsize=100000)
+result = 0
+for i in range(n):
+    cards.put(int(input()))
+
+while cards.qsize() > 1:  # 카드 모음이 2개 이상일때(카드 모음이 1개 남을때까지)
+
+    card_1 = cards.get()  # 가장 작은 수
+    card_2 = cards.get()  # 두번째로 작은 수
+    ssum = card_1+card_2
+    cards.put(ssum)
+    result += ssum
+print(result)
