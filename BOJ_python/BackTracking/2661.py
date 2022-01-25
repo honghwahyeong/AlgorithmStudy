882d003dce3955b1ae82d1c16407b891bcacfe40
honghwahyeong <ghdghkgud@dgu.ac.kr>
2022-01-26 01:40:29 +0900

    2661_2

--- BOJ_python/BackTracking/2661.py
+++ BOJ_python/BackTracking/2661.py
@@ -1,49 +1,25 @@
-# n = int(input())
-# board = []
-
-# def check(arr,number):
-#     arr += number
-#     for i in range(2,len(arr)//2+1):
-#         for k in range()
-#         for j in range(i,len(arr),i):
-#             if arr[:j]
-
-
-# def sol(cnt,num_arr):
-#     global board
-#     if cnt == n:
-#         board.append(int(num_arr))
-#         return
-#     for i in range(1,4): # 1,2,3 한개씩 넣어보면서
-#         if check: # check 함수로 좋은 수열이 맞으면 계속 진행
-#             sol(cnt+1,num_arr+str(i))
-
-
-# sol(1,'1')
-# board_int = list(map(int, board))
-# board_int.sort()
-# print(board_int[0])
+import sys
 
 n = int(input())
-board = []
 
 
-def sol(idx):
+def check(arr, number):
+    arr += number
+    for i in range(1, len(arr)//2+1):
+        for j in range(i, len(arr) - i+1):
+            if arr[j-i:j] == arr[j:j+i]:
+                return False
+    return True
+
+
+def sol(cnt, num_arr):
     global board
-    for i in range(1, (idx//2)+1):
-        if board[-i:] == board[-2*i:-i]:
-            return -1
-
-        if idx == n:
-            for i in range(n):
-                print(board[i], end='')
-            return 0
-
-        for i in range(1, 4):
-            board.append(i)
-            if sol(idx+1) == 0:
-                return 0
-            board.pop()
+    if cnt == n:
+        print(num_arr)
+        sys.exit()
+    for i in range(1, 4):  # 1,2,3 한개씩 넣어보면서
+        if check(num_arr, str(i)):  # check 함수로 좋은 수열이 맞으면 계속 진행
+            sol(cnt+1, num_arr+str(i))
 
 
-sol(0)
+sol(1, '1')
